afa05f089e51b87dee6c6ef505de93299f503080
LeoGouchon <72149969+LeoGouchon@users.noreply.github.com>
2020-11-27 11:33:28 +0100

    Delete ppTkinter.py

--- ppTkinter.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-window = tk.Tk()
-labelHello = tk.Label(window, text = 'hello world', fg = "green")
-labelHello.pack()
-
-buttonQuitt = tk.Button(window, text = "EXIT", fg = "red", command = window.destroy
