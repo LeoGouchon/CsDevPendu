1935ace5a963825c78ca7750b096ce613a74a97c
Léo Gouchon <leo.gouchon@gmail.com>
2020-11-27 10:15:40 +0100

    optimisation du code

--- pgrmPrincipal.py
+++ pgrmPrincipal.py
@@ -1,14 +1,11 @@
 import fPendu
 
-#Initialisation de la partie
-word = fPendu.pickWord()
-lstLetterUsed = [word[0]]
-hp = 8
-print('nbr de vie : ',hp)
-print(fPendu.displayWord(word, lstLetterUsed))
+#Personal Best Score
+pb = ""
 
-#Coeur du code
-while fPendu.finPartie(hp, word, lstLetterUsed) == False :
-    #on fait séléctionner une lettre
-    hp = fPendu.checkLife(hp, word, lstLetterUsed)
-    displayWord(word)
+hp=fPendu.lancerPartie()
+
+if hp == 0 :
+    print("Vous avez perdu...")
+else :
+    print("Bravo vous avez gagné !")
