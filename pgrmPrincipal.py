388381b867f1377e59907741f1ea664b2a6b7b6f
Léo Gouchon <leo.gouchon@gmail.com>
2020-11-27 10:39:00 +0100

    optimisation du code

--- pgrmPrincipal.py
+++ pgrmPrincipal.py
@@ -1,11 +1,24 @@
 import fPendu
 
 #Personal Best Score
-pb = ""
+pb = 0
 
-hp=fPendu.lancerPartie()
+print("\nMeilleur score : ", pb)
+choice = ""
+while choice != 'oui' and choice != 'non':
+    choice = input("Voulez-vous jouer ?\noui\nnon\n>>> ")
+while choice == 'oui':
+    #lance une partie
+    hp=fPendu.partie()
+    #résultat de la partie
+    if hp == 0 :
+        print("Vous avez perdu...")
+    else :
+        if hp >= pb:
+            pb = hp
+            print(20*"\n"+"Nouveau record : ",pb)
+        print("Bravo vous avez gagné !")
+    print("meilleur score : ", pb)
+    #choix de l'utilisateur de continuer ou arreter
+    choice = input("\nVoulez-vous jouer ?\noui\nnon\n>>> ")
 
-if hp == 0 :
-    print("Vous avez perdu...")
-else :
-    print("Bravo vous avez gagné !")
